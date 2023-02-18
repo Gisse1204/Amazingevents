c5a339c0026ba280a90bf596b411158bc7fb54b7
Gisse1204 <gise_17@hotmail.com>
2023-02-17 23:02:35 -0300

    arreglos en nav, details y practica js

---
+++ scripts/details.js
@@ -0,0 +1,104 @@
+/* let minombre = prompt('miNombre');
+console.log(minombre);
+console.log(typeof minombre);
+
+let miapellido = prompt('miApellido');
+console.log(miapellido);
+console.log(typeof miapellido);
+
+let miedad = Number (prompt('miEdad'));
+console.log(miedad);
+console.log(typeof miedad);
+
+let mimascota = prompt('miMascota');
+console.log(mimascota);
+console.log(typeof mimascota);
+
+const edadmascota = Number (prompt('edadMascota'));
+console.log(edadmascota);
+console.log(typeof edadmascota);
+
+let nombrecompleto = "Me llamo " + minombre + " " + miapellido;
+alert(nombrecompleto);
+
+let textoPresentacion = "Me llamo " + minombre + " " + miapellido + ", tengo " + miedad + " años. Mi mascota se llama " + mimascota + " y tiene " + edadmascota + " años";
+alert(textoPresentacion);
+
+let sumaEdades = miedad + edadmascota;
+alert("Nuestras edades suman " + sumaEdades);
+
+let restaEdades = miedad - edadmascota;
+alert("La diferencia de nuestras edades es " + restaEdades);
+
+let productoEdades = miedad * edadmascota;
+alert("El producto de nuestras edades es " + productoEdades);
+
+let divisionEdades = miedad / edadmascota;
+alert("La división de nuestras edades es " + divisionEdades); */
+
+/* let alumno = {
+    nombre: "Gisela",
+    apellido: "Martínez",
+    edad: 39,
+    educacion: "Universitaria",
+    profesion: "Contadora",
+};
+
+console.table(alumno);
+console.log(alumno.nombre);
+console.log(alumno.apellido);
+console.log(alumno.edad);
+console.log(alumno.educacion);
+console.log(alumno.profesion); */
+
+/* let mascota = {
+    nombre: "Becky",
+    apellido: "Martínez",
+    edad: 6,
+    raza: "adoptada",
+    alimento: "carne y verduras",
+};
+
+console.table(mascota);
+console.log(mascota.nombre);
+console.log(mascota.apellido);
+console.log(mascota.edad);
+console.log(mascota.raza);
+console.log(mascota.alimento); */
+
+/* let frutas = ["cereza", "durazno", "banana", "melón", "frutilla",];
+
+console.log(frutas);
+
+let numeros = ["12", "148", "6418", "84589", "659483",];
+
+console.log(numeros);
+
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i]);
+}
+
+let familia = [
+    {nombre: "Gisela", edad: 39},
+    {nombre: "Juan Manuel", edad: 40},
+    {nombre: "Mariana", edad: 39},
+    {nombre: "Lorena", edad: 39},
+    {nombre: "Julio", edad: 40},
+];
+
+console.log(familia);
+
+for (let i = 0; i < familia.length; i++) {
+    console.log(familia[i]);
+}
+
+let textoAleatorio = 
+familia[4].nombre + 
+" de " + 
+familia[4].edad + 
+" años de edad, ha comido " + 
+numeros[3] + 
+" " + 
+frutas[1];
+
+console.log(textoAleatorio); */
