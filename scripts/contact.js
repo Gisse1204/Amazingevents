d797059242ab39aae3d2e81f16b4537064d67b32
Gisse1204 <gise_17@hotmail.com>
2023-03-15 17:57:14 -0300

    arreglo en contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,27 +1,29 @@
 function EnviarFormulario() {
-    let name = document.getElementById('inputName').value;
-    let email = document.getElementById('inputEmail').value;
-    let message = document.getElementById('inputMessage').value;
+    try {
+        let name = document.getElementById('inputName').value;
+        let email = document.getElementById('inputEmail').value;
+        let message = document.getElementById('inputMessage').value;
 
-    if (name && email && message) {
-        console.log('Name:', name);
-        console.log('Email:', email);
-        console.log('Message:', message);
-    
-        swal({
-            title: "Message sent successfully!",
-            icon: "success",
-            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-        }).then(() => {
-            location.reload();
-        });
-    } else {
+        if (name && email && message) {
+            console.log('Name:', name);
+            console.log('Email:', email);
+            console.log('Message:', message);
+
+            swal({
+                title: "Message sent successfully!",
+                icon: "success",
+                text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+            }).then(() => {
+                location.reload();
+            });
+        } else {
+            throw new Error("Please fill in all fields");
+        }
+    } catch (error) {
         swal({
             title: "Error",
-            text: "Please fill in all fields",
+            text: error.message,
             icon: "error"
         });
     }
 }
-
-    // swal("Message sent successfully!", 'succes');  así lo estaría usando como una cadena de texto, está mal
