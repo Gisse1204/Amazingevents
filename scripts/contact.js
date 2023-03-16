f6670c4a833cd6c77022bb385add073bc37b3a90
Gisse1204 <gise_17@hotmail.com>
2023-03-16 20:30:31 -0300

    inicio

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,12 +3,10 @@ function EnviarFormulario() {
         let name = document.getElementById('inputName').value;
         let email = document.getElementById('inputEmail').value;
         let message = document.getElementById('inputMessage').value;
-
         if (name && email && message) {
             console.log('Name:', name);
             console.log('Email:', email);
             console.log('Message:', message);
-
             swal({
                 title: "Message sent successfully!",
                 icon: "success",
